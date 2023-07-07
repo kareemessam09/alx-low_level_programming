e8766b293859867b1588c393e8e5c2e5030294d3
KemOn09 <65365457+kareemessam09@users.noreply.github.com>
2023-07-07 23:19:55 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,33 @@
 #include "main.h"
 /**
- * binary_to_uint - Entry Point
- * @b: const char
- * Return: 0
+ * binary_to_uint - converts binary number to an unsigned int
+ * @b: a string of 0 and 1 chars
+ *
+ * Return: converted number or 0 if b is NULL or different from 0 or 1
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int base = 1, i = 0;
+	int length, i, add;
+	add = 0;
+	if (b != NULL)
+	{
+		for (length = 0 ; b[length] != '\0' ; length++)
+		{
+			if (b[length] != 48 && b[length] != 49)
+			{
+				return (0);
+			}
+		}
 
-	if (b == NULL)
+		for (i = 0, length-- ; length >= 0 ; length--)
+		{
+			add = add + ((b[length] - '0') << i);
+			i++;
+		}
+	}
+	else
+	{
 		return (0);
-
-	while (b[i + 1])
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		i++;
 	}
-
-	while (i >= 0)
-	{
-		res += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
-	}
-
-
-	return (res);
-
+	return (add);
 }
